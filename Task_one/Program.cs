055a51ef477e215826960235279d2111d803acd9
Dmyrix <mangoost81@gmail.com>
2023-12-08 22:15:25 +0300

    add types of variable

--- Task_one/Program.cs
+++ Task_one/Program.cs
@@ -3,3 +3,10 @@
 
 int value = 100;
 Console.WriteLine(value);
+
+double d = 5.55;   //переменная вещественного типа
+char c = '!';     //переменная символьного типа 
+bool b = true;     // переменная логического типа
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
