851e0b42705c267e160c1f92b2dd92b5cf287fcc
Dmyrix <mangoost81@gmail.com>
2023-12-09 19:33:16 +0300

    complete

--- Task_four/Program.cs
+++ Task_four/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 7;
+int b = 9;
+int c = 4;
+int d = 6;
+int e = 7;
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+if(d > max)
+{
+    max = d;
+}
+if(e > max)
+{
+    max = e;
+}
+
+Console.Write("Maximum is ");
+Console.WriteLine(max);
