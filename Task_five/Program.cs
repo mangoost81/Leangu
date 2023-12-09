5d938237a07296e406cb31d6926485145c9c8916
Dmyrix <mangoost81@gmail.com>
2023-12-09 21:43:57 +0300

    finish task

--- Task_five/Program.cs
+++ Task_five/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 100;
+int i = 1;
+while (i <= n)
+{
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
